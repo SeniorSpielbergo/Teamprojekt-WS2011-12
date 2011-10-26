abace2ead1b82e2cdd68207ab5a5eca442ece01e
Nils Breyer <nils.breyer@me.com>
2011-10-26 13:51:21 +0200

    pc test

--- src/nxt/Master.java
+++ src/nxt/Master.java
@@ -4,30 +4,30 @@ import lejos.nxt.comm.*;
 import java.io.*;
 import javax.bluetooth.*;
 public class Master {
-  public static void main(String[] args) throws Exception {
-	  RemoteNXT nxt = null;
-	  try {
-		  LCD.drawString("Connecting...",0,0);
-		  //RemoteDevice btrd = Bluetooth.getKnownDevice("NXT_03");
-		  //BTConnection btc = Bluetooth.connect(btrd);
+	public static void main(String[] args) throws Exception {
+		RemoteNXT nxt = null;
+		try {
+			LCD.drawString("Connecting...",0,0);
+			//RemoteDevice btrd = Bluetooth.getKnownDevice("NXT_03");
+			//BTConnection btc = Bluetooth.connect(btrd);
 
-		  nxt = new RemoteNXT("NXT_03",Bluetooth.getConnector());
-	
-		  LCD.clear();
-		  LCD.drawString("Connected",0,0);
+			nxt = new RemoteNXT("NXT_03",Bluetooth.getConnector());
+
+			LCD.clear();
+			LCD.drawString("Connected",0,0);
 		} catch (IOException ioe) {
-		  LCD.clear();
-		  LCD.drawString("Conn Failed",0,0);
-		  Button.waitForPress();
-		  System.exit(1);
+			LCD.clear();
+			LCD.drawString("Conn Failed",0,0);
+			Button.waitForPress();
+			System.exit(1);
 		}
-	  nxt.A.forward();
-	  try {
-		  Thread.sleep(1000);
-	  }
-	  catch (Exception e) {
-		  
-	  }
-  }
+		nxt.A.forward();
+		try {
+			Thread.sleep(1000);
+		}
+		catch (Exception e) {
+
+		}
+	}
 }
 
