07af6f7a11328493f46f243f8f19a3015d3f98ca
David Wille <david.wille@ewetel.net>
2011-10-28 17:14:58 +0200

    clean up

--- src/nxt/NXTConnector.java
+++
@@ -1,7 +0,0 @@
-import lejos.nxt.comm.NXTCommConnector;
-import javax.bluetooth.*;
-public class NXTConnector implements NXTCommConnector {
-	public NXTConnection connect(String target,int mode) {
-		return new BTConnection(Bluetooth.getKnownDevice(target));
-	}
-}
