69b7db19057a16cbd8df6b24c41af64c66d480d1
Nils Breyer <nils.breyer@me.com>
2011-10-28 15:28:50 +0200

    Test mit zwei NXTs

--- src/pc/Main.java
+++ src/pc/Main.java
@@ -12,29 +12,45 @@ public class Main {
 		catch (Exception e) {
 			
 		}
-		NXTInfo info = new NXTInfo(NXTCommFactory.BLUETOOTH, "IPS_03", "00:16:53:13:53:BB");
+		NXTInfo info_IPS_03 = new NXTInfo(NXTCommFactory.BLUETOOTH, "IPS_03", "00:16:53:13:53:BB");
+		NXTInfo info_NXT_03 = new NXTInfo(NXTCommFactory.BLUETOOTH, "NXT_03", "00:16:53:0F:DB:8E");
 		
 		try {
-			System.out.println("Connecting...");
+			System.out.println("Connecting to IPS_03...");
 
-			nxtComm.open(info);
+			nxtComm.open(info_IPS_03);
+			
+			InputStream is_IPS_03 = nxtComm.getInputStream();
+			OutputStream os_IPS_03 = nxtComm.getOutputStream();
 
-			InputStream is = nxtComm.getInputStream();
-			OutputStream os = nxtComm.getOutputStream();
+			DataInputStream in_IPS_03 = new DataInputStream(is_IPS_03);
+			DataOutputStream out_IPS_03 = new DataOutputStream(os_IPS_03);
 
-			DataInputStream in = new DataInputStream(is);
-			DataOutputStream out = new DataOutputStream(os);
+			System.out.println("Connected. Connecting to NXT_03...");
+			
+			nxtComm.open(info_NXT_03);
+			
+			InputStream is_NXT_03 = nxtComm.getInputStream();
+			OutputStream os_NXT_03 = nxtComm.getOutputStream();
 
-			System.out.println("Connected.");
+			DataInputStream in_NXT_03 = new DataInputStream(is_NXT_03);
+			DataOutputStream out_NXT_03 = new DataOutputStream(os_NXT_03);
+			
+			System.out.println("Connected. Ready.");
+
 			
 			while (true) {
 				String cmd = JOptionPane.showInputDialog("Enter command:");
 				if (cmd.equals("quit")) {
 					break;
 				}
-				else if (cmd.equals("test")) {
-					out.writeChar('t');
-					out.flush();
+				else if (cmd.equals("IPS_03:test")) {
+					out_IPS_03.writeChar('t');
+					out_IPS_03.flush();
+				}
+				else if (cmd.equals("NXT_03:test")) {
+					out_NXT_03.writeChar('t');
+					out_NXT_03.flush();
 				}
 			}
 			
