a15f342dd55984601f8cc53b2b2198dd38f81747
David Wille <david.wille@ewetel.net>
2011-10-28 13:18:16 +0200

    connecting with other nxt

--- src/nxt/Main.java
+++ src/nxt/Main.java
@@ -1,36 +1,51 @@
 import lejos.nxt.*;
 import lejos.nxt.addon.ColorSensor;
+import lejos.nxt.remote.*;
+import lejos.nxt.comm.*;
+import javax.bluetooth.*;;
+import java.io.*;
 
 /*
- * TODO: check values after calibration
  * black = 0
  * brown = 0
  * blue = 2
  * dark green = 4
- * light green = 6 (u)
- * yellow = 7 (u)
- * red = 9 (u)
- * orange = 9 (u)
- * grey = 14 (u)
- * white = 14 (u)
+ * light green = 6
+ * yellow = 7
+ * red = 9
+ * orange = 9
+ * grey = 14
+ * white = 14
  */
 
 public class Main {
 	static int counter = 0;
 	static ColorSensor counterSensor;
+	static RemoteNXT nxt_03;
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		try {
+			nxt_03 = new RemoteNXT("NXT_03", Bluetooth.getConnector());
+		}
+		catch (IOException e) {
+		}
 		final int PUSH_SPEED = 150;
 		final int LINE_SPEED = 250;
 		final int PUSH_ANGLE = -155;
+		final int PUSH_ANGLE_REMOTE = -165;
 		boolean grey = true;
 		boolean color = false;
 		String sensor1, sensor2, sensor3, counterString;
 		ColorSensor cs1 = new ColorSensor(SensorPort.S1);
 		ColorSensor cs2 = new ColorSensor(SensorPort.S2);
 		TouchSensor ts1 = new TouchSensor(SensorPort.S4);
+		// initialize speeds
 		Motor.A.setSpeed(LINE_SPEED);
 		Motor.B.setSpeed(PUSH_SPEED);
+		Motor.C.setSpeed(PUSH_SPEED);
+		nxt_03.B.setSpeed(PUSH_SPEED);
+		nxt_03.C.setSpeed(PUSH_SPEED);
+		//initialize counter
 		Counter c = new Counter();
 		c.start();
 		counterSensor = c.getCounterSensor();
@@ -71,6 +86,12 @@ public class Main {
 			public void buttonPressed(Button b) {
 				Motor.B.rotate(PUSH_ANGLE);
 				Motor.B.rotate(PUSH_ANGLE*(-1)+1);
+				Motor.C.rotate(PUSH_ANGLE);
+				Motor.C.rotate(PUSH_ANGLE*(-1)+1);
+				nxt_03.B.rotate(PUSH_ANGLE_REMOTE);
+				nxt_03.B.rotate(PUSH_ANGLE_REMOTE*(-1)+1);
+				nxt_03.C.rotate(PUSH_ANGLE_REMOTE);
+				nxt_03.C.rotate(PUSH_ANGLE_REMOTE*(-1)+1);
 			}
 			public void buttonReleased(Button b) {
 			}
