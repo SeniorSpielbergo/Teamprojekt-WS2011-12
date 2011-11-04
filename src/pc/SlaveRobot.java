fd44b660035521ca5084b121b8602d4d2420cee3
Philipp Neumann <ph.neumann@tu-bs.de>
2011-11-04 14:25:34 +0100

    Java Doc SlaveRobot.java

--- src/pc/SlaveRobot.java
+++ src/pc/SlaveRobot.java
@@ -5,6 +5,11 @@
  * @author Nils Breyer
  */
 public class SlaveRobot extends Robot {
+	/**
+	 * Constructor for a SlaveRobot
+	 * @param name Name of the SlaveRobot
+	 * @param mac_address MAC address of the SlaveRobot
+	 */
 	public SlaveRobot(String name, String mac_address) {
 		super(name,mac_address);
 	}
