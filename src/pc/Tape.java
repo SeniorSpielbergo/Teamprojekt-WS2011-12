0e615a92b5acdbd59582ea6adfe59dde8c09af59
Nils Breyer <nils.breyer@me.com>
2011-11-04 13:54:54 +0100

    javadoc

--- src/pc/Tape.java
+++ src/pc/Tape.java
@@ -1,3 +1,9 @@
+/**
+ * This abstract class is the common base for LEGO and Console/PC tapes. 
+ * It contains no concrete methods.
+ *
+ * @author Nils Breyer
+ */
 
 public abstract class Tape {
 	private char currentSymbol = 'n';
