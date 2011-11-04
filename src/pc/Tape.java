b0c34c481d9cbd4f678cb8916b968e256c58f705
Nils Breyer <nils.breyer@me.com>
2011-11-04 15:47:33 +0100

    javadoc for Tape

--- src/pc/Tape.java
+++ src/pc/Tape.java
@@ -9,17 +9,41 @@ public abstract class Tape {
 	private char currentSymbol = 'n';
 	boolean ready = false;
 	
+	/**
+	 * Initializes the tape
+	 * @throws Exception
+	 */
 	public abstract void init() throws Exception;
 	
+	/**
+	 * Shutdown the tape
+	 */
 	public abstract void shutdown();
 	
+	/**
+	 * Reads the symbol at the current tape position
+	 * @return
+	 */
 	public abstract char read();
 	
+	/**
+	 * Initializes the tape
+	 * @throws Exception
+	 */
 	public abstract void write(char c);
 	
+	/**
+	 * Move the tape one field to the left
+	 */
 	public abstract void moveLeft();
 	
+	/**
+	 * Move the tape one field to the right
+	 */
 	public abstract void moveRight();
 	
+	/**
+	 * This method runs a test on the tape. It is not specified what this method actually does.
+	 */
 	public abstract void test();
 }
