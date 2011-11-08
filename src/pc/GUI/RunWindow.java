53b1fbf3fef20c3e9b3d0d30f9fea8e7feccc6f9
Philipp Neumann <ph.neumann@tu-bs.de>
2011-11-08 13:19:14 +0100

    Action Listener in RunWindow

--- src/pc/GUI/RunWindow.java
+++ src/pc/GUI/RunWindow.java
@@ -1,9 +1,15 @@
 package GUI;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class RunWindow extends JFrame {
+
+public class RunWindow extends JFrame implements ActionListener {
 	public RunWindow() {
 		setTitle("Run");
 		setSize(600, 200);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 }
