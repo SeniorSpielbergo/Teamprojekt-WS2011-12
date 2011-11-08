81df0df950c0ea89dc21a320db43333f78b6b428
David Wille <david.wille@ewetel.net>
2011-11-08 17:54:47 +0100

    added labels and textfields

--- src/pc/GUI/RunWindow.java
+++ src/pc/GUI/RunWindow.java
@@ -14,32 +14,37 @@ public class RunWindow extends JFrame implements ActionListener {
 		expandWindowButton = new JButton("expand Options");
 		this.machine = machine;
 		JComboBox[] combo = new JComboBox[machine.getTapes()];
+		JLabel[] tapeLabel = new JLabel[machine.getTapes()];
+		JTextField[] tapeName = new JTextField[machine.getTapes()];
 		setTitle("Run");
 		setSize(600, 200);
 		
-		// set layout
 		Container contentPane = this.getContentPane();
 		JPanel inputContainer = new JPanel();
 		JPanel comboContainer = new JPanel();
+		JPanel expandContainer = new JPanel();
 		JTextField input = new JTextField(50);
 		inputContainer.add(input);
+		expandContainer.add(expandWindowButton);
 		comboContainer.setBorder(BorderFactory.createTitledBorder("Tapes"));
 		inputContainer.setBorder(BorderFactory.createTitledBorder("Input"));
 		expandWindowButton.addActionListener(this);
 		
+		// initialize combo boxes
 		for (int i = 0; i < combo.length; i++) {
 			combo[i] = new JComboBox();
+			tapeLabel[i] = new JLabel("Tape" + i + ":");
+			tapeName[i] = new JTextField(20);
+			
 			for (int j = 0; j < description.length; j++) {
 				combo[i].addItem(description[j]);
 			}
+			comboContainer.add(tapeLabel[i]);
+			comboContainer.add(tapeName[i]);
 			comboContainer.add(combo[i]);
 		}
 		
-		/*layout.setHorizontalGroup(
-		   layout.createSequentialGroup()
-		      .addComponent(combo[0])
-		      .addComponent(expandWindowButton)
-		);*/
+		// add to window and set layout
 		contentPane.add(inputContainer, BorderLayout.BEFORE_FIRST_LINE);
 		contentPane.add(comboContainer, BorderLayout.CENTER);
 	}
