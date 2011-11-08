c4f89d552bcecdde970ab61de339b6c9dbf1dd0f
Philipp Neumann <ph.neumann@tu-bs.de>
2011-11-08 13:41:15 +0100

    RunWindow expandWindowButton

--- src/pc/GUI/RunWindow.java
+++ src/pc/GUI/RunWindow.java
@@ -7,8 +7,12 @@ public class RunWindow extends JFrame implements ActionListener {
 
 	
 	protected TuringMachine machine;
+	public JButton expandWindowButton;
 	
 	public RunWindow(TuringMachine machine) {
+		expandWindowButton = new JButton("expand Options");
+		add(expandWindowButton);
+		expandWindowButton.addActionListener(this);
 		this.machine = machine;
 		JComboBox[] combo = new JComboBox[machine.getTapes()];
 		setTitle("Run");
@@ -16,6 +20,8 @@ public class RunWindow extends JFrame implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
+		if (e.getSource() == expandWindowButton) {
+			this.setSize(600, 500);
+		}
 	}
 }
