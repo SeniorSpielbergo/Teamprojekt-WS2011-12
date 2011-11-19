74ebfa54ddc442657cb97a682770e60cff8162e5
Nils Breyer <nils.breyer@me.com>
2011-11-20 00:07:29 +0100

    javadoc for the machine package

--- src/pc/machine/Machine.java
+++ src/pc/machine/Machine.java
@@ -8,16 +8,37 @@ import java.util.ArrayList;
 import tape.Tape;
 import tape.TapeException;
 
+/**
+ * An abstract class that represents a machine that operates on tapes
+ * @author Nils Breyer
+ *
+ */
 public abstract class Machine {
+	/**
+	 * An enum containing different machine types
+	 */
 	public enum MachineType {
-		TuringMachine, BrainfuckMachine
+		/**
+		 * A Turing machine
+		 */
+		TuringMachine, 
+		/**
+		 * A Brainfuck machine
+		 */
+		BrainfuckMachine
 	}
+	/**
+	 * The machine name string
+	 */
 	protected String name;
-
+	/**
+	 * The tapes array of the machine
+	 */
 	protected ArrayList<Tape> tapes;
-	
+	/**
+	 * The graphical editor for the machine
+	 */
 	protected MachineEditor editor;
-
 	/**
 	 * Constructs an untitled Turing machine
 	 */
@@ -75,20 +96,54 @@ public abstract class Machine {
 		return this.tapes;
 	}
 
+	/**
+	 * Returns the number of tapes of this machine
+	 * @return The tapes count
+	 */
 	public int getNumberOfTapes() {
 		return this.tapes.size();
 	}
 	
+	/**
+	 * Saves the machine code to a file on the disk
+	 * @param filename File where to save the machine code
+	 * @throws IOException Thrown if an I/O error occurs while writing to disk
+	 */
 	public abstract void save(String filename) throws IOException;
+	
+	/**
+	 * Loads the machine code from a file
+	 * This operation replaces the current code in the machine object with the code from the file.
+	 * @param filename A file that contains machine code for the fitting machine type
+	 * @throws IOException Thrown if an I/O error occours while reading the file or the file format is corrupt
+	 */
 	public abstract void load(String filename) throws IOException;
 	
+	/**
+	 * Returns a new Simulation object to simulate this machine
+	 * @return The simulation object
+	 * @throws TapeException Thrown if the simulation couldn't be created
+	 */
 	public abstract Simulation createSimulation() throws TapeException;
 	
+	/**
+	 * Returns a new graphical MachineEditor for this machine
+	 * Abstract method to be implemented by subclasses
+	 * @return The new MachineEditor
+	 */
 	protected abstract MachineEditor createEditor();
 	
+	/**
+	 * Returns the graphical MachineEditor for this machine
+	 * @return The MachineEditor
+	 */
 	public MachineEditor getEditor() {
 		return this.editor;
 	}
 	
+	/**
+	 * Returns type of the machine
+	 * @return The type of this machine
+	 */
 	public abstract MachineType getType();
 }
--- src/pc/machine/Simulation.java
+++ src/pc/machine/Simulation.java
@@ -2,14 +2,29 @@ package machine;
 
 import tape.TapeException;
 
+/**
+ * Abstract class that controls a simulation of a machine
+ * @author Vanessa Baier
+ *
+ */
 public abstract class Simulation {
-
+	/**
+	 * The machine to simulate
+	 */
 	Machine machine;
 	
+	/**
+	 * Creates a new Simulation object from a Machine object
+	 * @param machine The machine to simulate
+	 */
 	public Simulation(Machine machine){
 		this.machine = machine;
 	}
 	
+	/**
+	 * Runs the simulation
+	 * @throws TapeException Thrown, if a tape action fails during the simulation
+	 */
 	public abstract void runMachine() throws TapeException;
 	
 
