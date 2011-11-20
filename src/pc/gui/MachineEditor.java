03fcce0ba1a03f02cb44807c435013ba8c56c1dd
Nils Breyer <nils.breyer@me.com>
2011-11-20 16:19:26 +0100

    MachineEditor: javadoc

--- src/pc/gui/MachineEditor.java
+++ src/pc/gui/MachineEditor.java
@@ -2,6 +2,12 @@ package gui;
 
 import javax.swing.JPanel;
 
+/**
+ * Abstract base class for all graphical machine editors 
+ * @author Nils Breyer
+ *
+ */
+@SuppressWarnings("serial")
 public abstract class MachineEditor extends JPanel {
-	
+	//TODO: add methods for menu handling
 }
