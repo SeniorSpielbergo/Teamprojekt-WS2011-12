b4474be3ebb7801cba9d7f8672e8143edbceaccc
Nils Breyer <nils.breyer@me.com>
2011-11-21 21:50:58 +0100

    add jpgrah hello world to TuringMachine Editor for testing

--- src/pc/gui/turing/TuringMachineEditor.java
+++ src/pc/gui/turing/TuringMachineEditor.java
@@ -1,7 +1,40 @@
 package gui.turing;
 
+import javax.swing.JFrame;
+
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+
+
 import gui.MachineEditor;
 
 public class TuringMachineEditor extends MachineEditor {
+
+	public TuringMachineEditor() {
+		super();
+
+
+		mxGraph graph = new mxGraph();
+		Object parent = graph.getDefaultParent();
+
+		graph.getModel().beginUpdate();
+		try
+		{
+			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
+					30);
+			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
+					80, 30);
+			graph.insertEdge(parent, null, "Edge", v1, v2);
+		}
+		finally
+		{
+			graph.getModel().endUpdate();
+		}
+
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		this.add(graphComponent);
+	}
+
+
 
 }
