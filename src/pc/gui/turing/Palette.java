2045047d7b613b8a827d2a8d7260b2fc55f162bd
David Wille <david.wille@ewetel.net>
2011-11-25 12:17:55 +0100

    basic palette

---
+++ src/pc/gui/turing/Palette.java
@@ -0,0 +1,60 @@
+package gui.turing;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+
+public class Palette extends JPanel implements MouseListener {
+	
+	private static final long serialVersionUID = 2826433120960614428L;
+	private JLabel[] icons = new JLabel[4];
+
+	public Palette() {
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		icons[0] = new JLabel("System", new ImageIcon("gui/images/system.png"), JLabel.LEFT);
+		icons[1] = new JLabel("State", new ImageIcon("gui/images/state.png"), JLabel.LEFT);
+		icons[2] = new JLabel("Edge", new ImageIcon("gui/images/edge.png"), JLabel.LEFT);
+		icons[3] = new JLabel("Text", new ImageIcon("gui/images/text.png"), JLabel.LEFT);
+		for (int i = 0; i < 4; i++) {
+			c.fill = GridBagConstraints.BOTH;
+			c.gridx = 0;
+			c.gridy = i;
+			icons[i].addMouseListener(this);
+			this.add(icons[i], c);
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
