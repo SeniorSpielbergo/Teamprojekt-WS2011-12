0b75e87d656bd104f91615074f982afd0ed19606
Nessa Baier <v.baier@tu-bs.de>
2011-12-02 20:43:57 +0100

    git sucks!

--- src/pc/machine/Simulation.java
+++ src/pc/machine/Simulation.java
@@ -4,7 +4,7 @@ import gui.ErrorDialog;
 import tape.TapeException;
 
 /**
- * Abstract class that controls a simulation of a machine
+ * Abstract class that controls a simulation of a machine.
  * @author Vanessa Baier
  *
  */
