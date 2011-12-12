c8201258168fdce74d124a05473f324e25388a1f
Nils Breyer <nils.breyer@me.com>
2011-12-12 23:17:41 +0100

    about dialog improvements

--- src/pc/gui/AboutDialog.java
+++ src/pc/gui/AboutDialog.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -12,7 +13,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 public class AboutDialog extends JDialog {
 	JLabel jLabelIcon =  null;
@@ -21,7 +27,7 @@ public class AboutDialog extends JDialog {
 	JPanel jPanelAppName = null;
 	JLabel jLabelVersion = null;
 	JPanel jPanelVersion = null;
-	JLabel jLabelText = null;
+	JTextPane jTextPaneText = null;
 	JPanel jPanelText = null;
 
 
@@ -47,25 +53,42 @@ public class AboutDialog extends JDialog {
 		this.jPanelAppName = new JPanel(new BorderLayout());
 		this.jPanelAppName.add(this.jLabelAppName, BorderLayout.CENTER);
 		this.jPanelDetails.add(jPanelAppName);
-		this.jPanelDetails.add(Box.createRigidArea(new Dimension(0, 10)));
+		this.jPanelDetails.add(Box.createRigidArea(new Dimension(0, 5)));
 
 		
 		this.jLabelVersion = new JLabel(Editor.APP_VERSION, JLabel.CENTER);
 		this.jPanelVersion = new JPanel(new BorderLayout());
 		this.jPanelVersion.add(this.jLabelVersion, BorderLayout.CENTER);
 		this.jPanelDetails.add(jPanelVersion);
-		this.jPanelDetails.add(Box.createRigidArea(new Dimension(0, 20)));
+		this.jPanelDetails.add(Box.createRigidArea(new Dimension(0, 25)));
 
 		
-		this.jLabelText = new JLabel(Editor.APP_AUTHORS, JLabel.CENTER);
+		this.jTextPaneText = new JTextPane();
+		this.jTextPaneText.setText(Editor.APP_AUTHORS);
+		StyledDocument doc = this.jTextPaneText.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+		this.jTextPaneText.setEditable(false);
+		this.jTextPaneText.setOpaque(false);
 		this.jPanelText = new JPanel(new BorderLayout());
-		this.jPanelText.add(this.jLabelText, BorderLayout.CENTER);
+		this.jPanelText.add(this.jTextPaneText, BorderLayout.CENTER);
 		this.jPanelDetails.add(jPanelText);
 		this.jPanelDetails.add(Box.createRigidArea(new Dimension(0, 20)));
-
-		
+	
 		
 		this.setSize(new Dimension(300,400));
+		this.centerDialogOnTheScreen();
 	}
 	
+	/**
+	 * Center the dialog window on the screen
+	 */
+	private void centerDialogOnTheScreen() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension dialogSize = this.getSize();
+		int centerPosX = (screenSize.width - dialogSize.width) / 2;
+		int centerPosY = (screenSize.height - dialogSize.height) / 2;
+		setLocation(centerPosX, centerPosY);
+	}
 }
