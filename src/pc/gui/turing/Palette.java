9dab1c8cc6f066e0b6570c14bf3a3b0fc7c9afd9
Nils Breyer <nils.breyer@me.com>
2011-12-19 22:22:36 +0100

    prepare drag&drop

--- src/pc/gui/turing/Palette.java
+++ src/pc/gui/turing/Palette.java
@@ -3,10 +3,22 @@ package gui.turing;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
 
 import javax.swing.*;
+
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
+import com.mxgraph.swing.util.mxGraphTransferable;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxRectangle;
 
 public class Palette extends JPanel implements MouseListener {
 
@@ -21,12 +33,16 @@ public class Palette extends JPanel implements MouseListener {
 	public Palette() {
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		icons[0] = new JLabel("Frame", new ImageIcon("gui/images/frame.png"), JLabel.LEFT);
-		icons[1] = new JLabel("State", new ImageIcon("gui/images/state.png"), JLabel.LEFT);
-		icons[2] = new JLabel("Text", new ImageIcon("gui/images/text.png"), JLabel.LEFT);
-		icons[0].setName("Frame");
-		icons[1].setName("State");
-		icons[2].setName("Text");
+		mxCell cell = new mxCell(null, new mxGeometry(0, 0, 50, 50), null);//debug
+		cell.setVertex(true);
+		icons[0] = this.addIcon("Frame", "gui/images/frame.png", cell);
+		icons[1] = this.addIcon("State", "gui/images/state.png", cell);
+		icons[2] = this.addIcon("Text", "gui/images/text.png", cell);
+
+//		icons[0] = new JLabel("Frame", new ImageIcon("gui/images/frame.png"), JLabel.LEFT);
+//		icons[1] = new JLabel("State", new ImageIcon("gui/images/state.png"), JLabel.LEFT);
+//		icons[2] = new JLabel("Text", new ImageIcon("gui/images/text.png"), JLabel.LEFT);
+
 		for (int i = 0; i < 3; i++) {
 			c.fill = GridBagConstraints.BOTH;
 			c.gridx = i % 2;
@@ -36,6 +52,36 @@ public class Palette extends JPanel implements MouseListener {
 			this.add(icons[i], c);
 		}
 	}
+	
+	public JLabel addIcon(String name, String icon, mxCell cell) {
+		JLabel label = new JLabel(name, new ImageIcon(icon), JLabel.LEFT);
+		label.setName(name);
+		
+		mxRectangle bounds = (mxGeometry) cell.getGeometry().clone();
+		final mxGraphTransferable t = new mxGraphTransferable(
+				new Object[] { cell }, bounds);
+		
+		DragGestureListener dragGestureListener = new DragGestureListener()
+		{
+			/**
+			 * 
+			 */
+			public void dragGestureRecognized(DragGestureEvent e)
+			{
+				e
+						.startDrag(null, mxConstants.EMPTY_IMAGE, new Point(),
+								t, null);
+			}
+
+		};
+
+		DragSource dragSource = new DragSource();
+		dragSource.createDefaultDragGestureRecognizer(label,
+				DnDConstants.ACTION_COPY, dragGestureListener);
+		
+		return label;
+	}
+
 	
 	/**
 	 * Returns the clicked item
@@ -76,4 +122,6 @@ public class Palette extends JPanel implements MouseListener {
 	@Override
 	public void mouseReleased(MouseEvent e) {
 	}
+	
+	
 }
