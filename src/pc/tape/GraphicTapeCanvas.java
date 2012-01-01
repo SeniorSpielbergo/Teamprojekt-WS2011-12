d5bcd0b49d2d2e3ea1071d5886a746f7b8e66121
Nils Breyer <nils.breyer@me.com>
2012-01-01 17:26:42 +0100

    moved image, image test

--- src/pc/tape/GraphicTapeCanvas.java
+++ src/pc/tape/GraphicTapeCanvas.java
@@ -1,7 +1,13 @@
 package tape;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class GraphicTapeCanvas extends Canvas {
 	private Tape tape;
@@ -9,12 +15,22 @@ public class GraphicTapeCanvas extends Canvas {
 	public GraphicTapeCanvas(GraphicTape tape) {
 		super();
 		this.tape = tape;
+		this.setPreferredSize(new Dimension(50,50));
+		repaint();
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		super.paint(g);
+		System.out.println("Test");
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("tape/images/tape_field.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		g.drawImage(img, 0, 0, null);
 	}
-	
-	
+
+
 }
