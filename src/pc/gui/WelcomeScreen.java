69d75a065862ba8ea0d9f405afcdca020d0a68db
Nils Breyer <nils.breyer@me.com>
2012-01-28 17:58:22 +0100

    small change to new file icons, changed welcome page header

--- src/pc/gui/WelcomeScreen.java
+++ src/pc/gui/WelcomeScreen.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextPane;
 
 import gui.WelcomeScreenLine.Type;
 import machine.Machine.MachineType;
@@ -25,8 +26,10 @@ public class WelcomeScreen extends JPanel {
 	private JPanel turingContainer;
 	private JPanel brainfuckContainer;
 	private JPanel header;
+	private JPanel headerRight;
 	private JLabel logo;
-	private JLabel headerText;
+	private JLabel headerTitle;
+	private JTextPane headerText;
 	private JPanel turingOpen;
 	private JPanel turingCreate;
 	private JPanel turingExamples;
@@ -45,8 +48,20 @@ public class WelcomeScreen extends JPanel {
 
 		header.add(logo, BorderLayout.LINE_START);
 		this.logo.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		header.add(headerText, BorderLayout.CENTER);
-		this.headerText.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		header.add(headerRight, BorderLayout.CENTER);
+		
+		this.headerTitle.setFont(this.headerTitle.getFont().deriveFont(24f));
+		this.headerText.setEditable(false);
+		this.headerText.setBackground(this.getBackground());
+		this.headerText.setOpaque(true);
+		this.headerText.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+
+		
+		this.headerRight.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		headerRight.setLayout(new BorderLayout());
+		headerRight.add(this.headerTitle, BorderLayout.NORTH);
+		headerRight.add(this.headerText, BorderLayout.CENTER);
 		
 		GridBagConstraints c = new GridBagConstraints();
 		
@@ -130,6 +145,10 @@ public class WelcomeScreen extends JPanel {
 		brainfuckContainer = new JPanel();
 		logo = new JLabel("", new ImageIcon(this.getClass().getResource("images/logo.png")), JLabel.CENTER);
 		header = new JPanel();
-		headerText = new JLabel("Welcome to " + AppData.APP_NAME + "!");
+		headerRight = new JPanel();
+		headerTitle = new JLabel("Welcome to " + AppData.APP_NAME + "!");
+		headerText = new JTextPane();
+		headerText.setText(AppData.APP_NAME + " lets you create and simulate your own Turing Machines and Brainfuck programs. "
+				+ "If you are new to " + AppData.APP_NAME + " we recommend you to have a look at the example machines below.");
 	}
 }
